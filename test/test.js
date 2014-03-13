5a1c8d7212ea456bbc45eb14abd2e0cace340fbb
rogerthomas84 <rogere84@gmail.com>
2014-03-13 12:59:02 +0000

    Basic test

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var assert = require("assert")
+describe('String', function(){
+    describe('#foo', function(){
+        it('should equal bar', function(){
+            // equal(expected, actual)
+            assert.equal("foo", "foo");
+        })
+    })
+});
+
+describe('String', function(){
+    describe('#foo', function(){
+        it('should equal bar', function(){
+            // equal(expected, actual)
+            assert.equal("bar", "foo");
+        })
+    })
+});
